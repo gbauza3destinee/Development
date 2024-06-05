4b59649a10e74f06e38808a141b4191ae23443a4
Des G Bauza <dgb.javadeveloper@gmail.com>
2024-06-05 18:35:04 -0400

    AddingOperatorsToNotes

--- class2-print-variable-operators/class2_variables_and_operators.py
+++ class2-print-variable-operators/class2_variables_and_operators.py
@@ -1,2 +1,27 @@
 ## 1. Print Statement in Python 
+# Uncomment multiple lines at once command + /
 
+print("Hello")
+
+## Addition
+# print(3 + 3)
+
+#Subtraction 
+# print(10-7)
+
+#Multiplication
+# print(5*5)
+
+#Division 
+# print(20/10)
+# print(11/3)
+
+try: 
+    print(5/0)
+except ZeroDivisionError as e:
+    print("You cannot divide by 0")
+
+
+#Exponents 
+print(5**2)
+print(2**3)
