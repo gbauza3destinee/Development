9484d2fe2133b194bb4dae1d7c970614211986b4
Des G Bauza <dgb.javadeveloper@gmail.com>
2024-06-10 19:17:32 -0400

    Class2_BooleanAndOperatorShortcuts

---
+++ class3/class3_operatorsBooleanComments.py
@@ -0,0 +1,84 @@
+# Operators, Boolean Expressions and Comments
+
+# I am a single line comment 
+temperature = 95 #I am an inline comment 
+first_name = "Destinee" """
+Hello this is an inline multi-line comment! """
+
+#Comment 
+length = 5 # length of the rectangle
+width = 7 # width of the rectangle 
+perimeter = 2 * (length + width) #calculating the perimeter of rectangle
+# print(perimeter) 
+
+""" Converts a fahrenheit temperature 
+to a celsius temperature
+Using an equation """
+
+fahrenheit = 90
+celsius = (fahrenheit - 32) * 5/9
+# print(celsius)
+
+""" Shortcut Operators """
+#Add 5 to me 
+age = 25
+age += 5
+# print(age)
+
+# Add 10 years to 2024
+year = 2024
+year+=10
+# print(year)
+
+#Subtract 20
+num_one = 55
+num_one -=20
+# print(num_one)
+
+#Subtract 15
+num_two = 11
+num_two -= 15
+# print(num_two)
+
+#Multiply by 3 
+my_value = 9
+my_value *=3
+# print(my_value)
+
+#Multiply by 10 
+mileage = 15 
+mileage *=10
+# print(mileage)
+
+#Divide by 2 
+pizza_slices = 8
+pizza_slices /= 2
+# print(pizza_slices)
+
+#Divide by 7 
+fees = 8.90 
+fees /= 7
+# print(fees)
+
+#Raise to the 3rd power
+num_three = 6
+num_three **=3
+print(num_three)
+
+#Raise to the 2nd power **
+data = 16 
+data **=2
+print(data)
+
+#Integer division, how many times does 3 go into 16?
+val_one = 16
+val_one //=3
+print(val_one)
+
+## Integer divde by 4 //
+val_two = 9 
+val_two //= 4
+print(val_two)
+
+# Modulus we use often to find if a value is odd or even
+
