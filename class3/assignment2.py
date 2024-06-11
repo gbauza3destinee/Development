8b380f77ff442212a2c692f13570f06f965beccf
Des G Bauza <dgb.javadeveloper@gmail.com>
2024-06-10 20:48:35 -0400

    SubmittingClass3Assignment

--- class3/assignment2.py
+++ class3/assignment2.py
@@ -13,3 +13,12 @@ Requirements
 
 print(f"ENTRYGOESHERE{variable}")
 """
+
+base = 20
+height = 15
+#Equation is base * height * 1/2
+area = (base * height)* 1/2
+#String formatted 
+print(f"The area of a rectangle with{base} inches and {height} inches 
+      is : {area}")
+
