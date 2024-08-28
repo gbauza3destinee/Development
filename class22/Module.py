b4292486cd0fe8e1388ea4b5a25e611c898ae531
Des G Bauza <dgb.javadeveloper@gmail.com>
2024-08-27 21:04:11 -0400

    Str, Accessor, Mutator methods work

--- class22/Module.py
+++ class22/Module.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import pandas as pd
+
 
 
 """
@@ -15,8 +17,10 @@ Create an Employee class
  - Use type hinting in constructor 
  
 """
-
 class Employee: 
+
+    ### current_year(int) - self.hire_year (int)
+    ### what do I use from with for dataframe 
 
     ## FIELDS 
     # name: string​
@@ -25,7 +29,7 @@ class Employee:
     # salary: float​
     # hire_year: int or string​ 
 
-    def __init__(self, name:str, job_title:str, department:str, salary:float, hire_year:str):
+    def __init__(self, name:str, job_title:str, department:str, salary:int, hire_year:int):
         self.name = name
         self.job_title = job_title
         self.department = department
@@ -36,25 +40,55 @@ class Employee:
     def __str__(self):
         return f"{self.name}\n{self.job_title}\n{self.department}\n{self.salary}\n{self.hire_year}"
     
-    ## Accessor Mutator Methods
+    ## Accessor & Mutator Methods
     def get_name(self):
         return self.name
     
     def set_name(self, name:str):
         self.name = name
 
-    ### Years Worked
-    def years_worked( date):
-        current_year = datetime.now().year
-        return current_year - date
+    def get_job_title(self):
+        return self.job_title
     
-    ## Total Expense 
+    def set_job_title(self, job_title: str):
+        self.job_title = job_title
+    
+    def get_department(self):
+        return self.department
+    
+    def set_department(self, department: str):
+        self.department = department
+    
+    def get_salary(self):
+        return self.salary
+    
+    def set_salary(self, salary: int):
+        self.salary = salary
+    
+    def get_hire_year(self):
+        return self.hire_year
+    
+    def set_hire_year(self, hire_year: int):
+        self.hire_year = hire_year
+
+    ####################################### Business Logic ###########################
+
+    ### Years Worked
+    def years_worked(self):
+        current_year = datetime.now().year
+        return current_year - self.hire_year
+    
+    ## Total Expense
     def total_expense(self, salary):
-        years = self.years_worked(self.hire_date)
+        years = self.years_worked(self.hire_year)
         return years * salary
     
     ###### print employee name, job title, department, salary, 
     # and hire year to a text file named after the employee.
 
-    def print_emp_information(employee):
+    def print_emp_information(self):
+        employee_dict = self.__dict__
+        df = pd.DataFrame.from_dict(employee_dict)
+        df.to_csv('employees.csv', index=False)
+
 
--- class22/test_project2.py
+++ class22/test_project2.py
@@ -1,3 +1,21 @@
 from Module import Employee
 
-Employee emp = new Employee;
+emp = Employee("Sally", "Programmer Analyst","Tech","50000","05-03-2020")
+
+### Test 4 methods + accessor/mutator methods
+
+## Print name                                --- Works 
+# print(emp.get_name())
+# print(emp.get_hire_year())
+
+## Open CSV file with employee information  --- Faulty 
+# emp.print_emp_information()
+
+## Calculate from int to int years worked  --- Test
+# print(emp.years_worked())
+
+## Calculate total $ amount of years employee has worked 
+# print(emp.total_expense())
+
+## Print to String method -- Works 
+# print(emp.__str__())
